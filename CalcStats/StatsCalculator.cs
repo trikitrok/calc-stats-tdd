4ad7e6173349e1b5155799fe1ce76709c6cad91d
trikitrok <trikitrok@gmail.com>
2024-04-05 01:43:47 +0200

    refactor: generalizing average computation for any sequence

--- CalcStats/StatsCalculator.cs
+++ CalcStats/StatsCalculator.cs
@@ -12,7 +12,7 @@ public class StatsCalculator
             min: numbers.Count == 2 ? numbers[1]: numbers[0],
             max: numbers[0],
             num: numbers.Count,
-            avg: ((numbers.Count == 2 ? numbers[1]: 0) + numbers[0])/(decimal)numbers.Count
+            avg: numbers.AsQueryable().Sum()/(decimal)numbers.Count
         );
     }
 }
