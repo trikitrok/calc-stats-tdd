56d820a0a601d3b2e2e85ddd91b016227767c8d4
trikitrok <trikitrok@gmail.com>
2024-04-05 01:38:33 +0200

    refactor: parameterizing test

--- CalcStatsTests/StatsCalculatorTest.cs
+++ CalcStatsTests/StatsCalculatorTest.cs
@@ -34,18 +34,19 @@ public class StatsCalculatorTest
     }
     
     [Test]
-    public void Computes_Stats_For_A_Sequence_Containing_Two_Elements()
+    [TestCase(0,0, 0)]
+    public void Computes_Stats_For_A_Sequence_Containing_Two_Elements(int value1, int value2, int expectedAvg)
     {
-        List<int> numbers = new() { 0, 0 };
+        List<int> numbers = new() { value1, value2 };
 
         var stats = StatsCalculator.Calculate(numbers);
 
         Assert.That(stats, Is.EqualTo(
             new Stats(
-                min: 0,
-                max: 0,
+                min: value2,
+                max: value1,
                 num: 2,
-                avg: 0
+                avg: expectedAvg
             )));
     }
 }
